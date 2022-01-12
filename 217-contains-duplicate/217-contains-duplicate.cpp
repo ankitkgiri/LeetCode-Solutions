5f3140bc61dd5dc948ba42e26c810907c6170dde
ankitkgiri <55329837+ankitkgiri@users.noreply.github.com>
2022-01-12 23:53:31 +0530

    Time: 183 ms (7.05%), Space: 53.6 MB (9.05%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -3,12 +3,8 @@ public:
     bool containsDuplicate(vector<int>& nums) {
        unordered_map<int, int> ump;
         
-        for (int a: nums){
-            if (ump[a] > 0) {
-                return true;
-            }
-            ump[a]++;
-        }
-        return false;
+        for (int a: nums) ump[a]++;
+        
+        return nums.size() > ump.size();
     }
 };
