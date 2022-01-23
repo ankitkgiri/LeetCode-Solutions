b086065c12e98b89151e5eb8073ea139999cf23b
ankitkgiri <ankit.m.giri24@gmail.com>
2022-01-23 21:58:15 +0530

    Time: 100 ms (61.42%), Space: 6 MB (57.60%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -7,25 +7,25 @@ public:
         
         for (int i = 1; i <= n; i++) arr[i] = i;
         
-        permute(arr, n);
+        permute(arr, 1);
         
         return cnt;
     }
     
     void permute(vector<int>&arr, int idx) {
         
-        if (idx == 0){
+        if (idx == arr.size()){
             cnt++;
             return;
         }
         
         
-        for (int i = idx; i > 0; i--){
+        for (int i = idx; i < arr.size(); i++){
             
             swap(arr[i], arr[idx]);
             
             if (idx % arr[idx] == 0 || arr[idx] % idx == 0){
-                permute(arr, idx - 1);
+                permute(arr, idx + 1);
             }
             
             swap(arr[i], arr[idx]);
