ef030efa393fe02f62db07333a1c1bcfcb9a9ffd
ankitkgiri <ankit.m.giri24@gmail.com>
2022-02-10 11:49:48 +0530

    Time: 59 ms (35.61%), Space: 27.6 MB (63.72%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        
+        int lo = 0;
+        int hi = nums.size() - 1;
+        
+        while (lo <= hi) {
+            int mid = (lo + hi)/2;
+            
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]){
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+};
