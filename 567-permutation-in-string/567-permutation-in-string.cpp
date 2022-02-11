3fa2beedc6a86dbb196cd411cb6d9caa815959a4
ankitkgiri <ankit.m.giri24@gmail.com>
2022-02-11 11:37:46 +0530

    Time: 26 ms (25.18%), Space: 8.1 MB (17.14%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -4,32 +4,26 @@ public:
         
         int k = s1.length();
         
-        unordered_map<char, int> check;
-        unordered_map<char, int> mp;
+        unordered_map<char, int> check, mp;
         
         for (auto ch: s1) check[ch]++; 
         
         int start = 0;
-        
         for (int end = 0; end < s2.length(); end++) {
             
             mp[s2[end]]++;
             
             if (end+1 - start > k) {
-                mp[s2[start]]--;
                 
-                if (mp[s2[start]] == 0) {
-                    mp.erase(s2[start]);
-                }
+                mp[s2[start]]--;
+                if (mp[s2[start]] == 0) mp.erase(s2[start]);
                 
                 start++;
             }
             
             if (end+1 - start == k){
 
-                    if (mp == check){
-                        return true;
-                    }
+                if (mp == check) return true;
             }
             
         }
