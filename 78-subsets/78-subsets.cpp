185e43be288ade689cf1d2c5e6d99578d7d1d32b
ankitkgiri <ankit.m.giri24@gmail.com>
2022-02-13 11:25:25 +0530

    Time: 17 ms (5.50%), Space: 16 MB (18.06%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -3,21 +3,21 @@ public:
     vector<vector<int>> subs;
     
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> set;
+           
+        generate(nums, 0, vector<int>());
         
-        generate(nums, 0, set);
         return subs;
     }
     
     
     void generate(vector<int>& nums, int i, vector<int> set){
-        if (i >= nums.size()){
+      
+        if (i == nums.size()){
             subs.push_back(set);
             return;
         }
         generate(nums, i + 1, set);
         set.push_back(nums[i]);
         generate(nums, i + 1, set);
-        
     }
 };
